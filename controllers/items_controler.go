d2123eea78f25a5db5c335977279bec48fef219f
Paweł Joachimczuk <pawel@MacBook-Pro-Pawe.local>
2021-05-15 18:26:39 +0200

    tasks v2

--- controllers/items_controler.go
+++ controllers/items_controler.go
@@ -1,9 +1,15 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/Pawelek242/home_oauth-go/oauth"
+)
 
 func Create(w http.ResponseWriter, r *http.Request) {
-	//need to create authenticate
+	if err := oauth.AuthenticateRequest(r); err != nil {
+		return
+	}
 }
 
 func Get(w http.ResponseWriter, r *http.Request) {
